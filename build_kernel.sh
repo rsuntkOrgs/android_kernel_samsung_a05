a245b9c247abf2f75f7fcecf4ffd010df26908b6
Rissu J <rissu.ntk@gmail.com>
2024-10-17 16:56:40 +0000

    Use proton clang

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-export PATH=/rsuntk/env/clang-11/bin:$PATH
-export CROSS_COMPILE=/rsuntk/env/gcc-6.4/bin/aarch64-linux-gnu-
-export CC=/rsuntk/env/clang-11/bin/clang
+export PATH=$(pwd)/toolchain/bin:$PATH
+export CROSS_COMPILE=$(pwd)/toolchain/bin/aarch64-linux-gnu-
+export CROSS_COMPILE_ARM32=$(pwd)/toolchain/bin/arm-linux-gnueabi-
+export CC=$(pwd)/toolchain/bin/clang
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export ARCH=arm64
 export ANDROID_MAJOR_VERSION=u
 
 make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y LLVM=1 LLVM_IAS=1 rsuntk_defconfig
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y LLVM=1 LLVM_IAS=1 -j2
+make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y LLVM=1 LLVM_IAS=1 -j$(nproc --all)
